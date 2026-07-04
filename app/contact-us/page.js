681dbc8b4c6403aabecac43e6895237999d845f5
Monu Rohila <monu.rohila@gmail.com>
2026-07-04 10:40:55 +0530

    Delete app directory

--- app/contact-us/page.js
+++
@@ -1,2 +0,0 @@
-import {Layout, ContactPage} from '../../components/Site';
-export default function Page(){return <Layout><ContactPage/></Layout>}
--- app/sitemap.js
+++
@@ -1,2 +0,0 @@
-const routes=['','about','certification-courses','computer-courses','results','gallery','admissions','blog','contact','privacy-policy'];
-export default function sitemap(){return routes.map(r=>({url:`https://softech-demo.vercel.app/${r}`,lastModified:new Date()}))}
